b475eb68889cb0d03ec3fb19196abb2fed954484
Adam Anderson <84adam@users.noreply.github.com>
2019-12-04 17:53:28 -0800

    Create winscout-full.bat

---
+++ winscout-full.bat
@@ -0,0 +1,47 @@
+@echo off
+@setlocal
+
+REM This script tracks running time and saves output from `winscout-main.bat`.
+title Winscout Diagnoser
+
+REM timestamp
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "timestamp=%datestamp%_%timestamp%"
+
+REM set filename with timestamp, save to current directory
+set "filename=output-%timestamp%.txt"
+set mypath=%cd%
+
+REM script start time
+set start=%time%
+
+REM ############################################################
+REM run winscout-main.bat, echo results, and save to %filename%
+winscout-main.bat | tee.bat %filename%
+REM ############################################################
+
+REM calculate total time to execute
+set end=%time%
+set options="tokens=1-4 delims=:.,"
+for /f %options% %%a in ("%start%") do set start_h=%%a&set /a start_m=100%%b %% 100&set /a start_s=100%%c %% 100&set /a start_ms=100%%d %% 100
+for /f %options% %%a in ("%end%") do set end_h=%%a&set /a end_m=100%%b %% 100&set /a end_s=100%%c %% 100&set /a end_ms=100%%d %% 100
+set /a hours=%end_h%-%start_h%
+set /a mins=%end_m%-%start_m%
+set /a secs=%end_s%-%start_s%
+set /a ms=%end_ms%-%start_ms%
+if %ms% lss 0 set /a secs = %secs% - 1 & set /a ms = 100%ms%
+if %secs% lss 0 set /a mins = %mins% - 1 & set /a secs = 60%secs%
+if %mins% lss 0 set /a hours = %hours% - 1 & set /a mins = 60%mins%
+if %hours% lss 0 set /a hours = 24%hours%
+if 1%ms% lss 100 set ms=0%ms%
+set /a totalsecs = %hours%*3600 + %mins%*60 + %secs%
+
+REM print total running time and path to output file.
+echo DONE.
+echo Total Running Time: %hours% hrs, %mins% min, %secs%.%ms% sec (%totalsecs%.%ms%s)
+echo Diagnostic results saved to "%mypath%" as "%filename%"
+
+pause
