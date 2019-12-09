032d5e5293f56c750e72e609168aff14d8166328
Adam Anderson <84adam@users.noreply.github.com>
2019-12-09 10:43:57 -0800

    Update winscout-main.bat

--- winscout-main.bat
+++ winscout-main.bat
@@ -1,14 +1,31 @@
 @echo off
 
 REM This batch file captures OS, hardware, and networking configuration/status.
-title Winscout
-echo Checking system information...
+title Winscout Diagnoser
 
 REM Horizontal Rules:
+set x=##########
+set x=%x%%x%%x%%x%%x%%x%%x%%x%
 set y=----------
 set y=%y%%y%%y%%y%%y%%y%%y%%y%
 set z=__________
 set z=%z%%z%%z%%z%%z%%z%%z%%z%
+
+REM timestamp
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "timestamp=%datestamp%_%timestamp%"
+
+echo %x%
+echo %x%
+echo WINSCOUT DIAGNOSTIC SCAN
+echo SCAN START TIME: %timestamp%
+echo %x%
+echo %x%
+echo.
+echo Checking system information...
 
 REM Section 1: System and OS information.
 echo %y%
@@ -39,7 +56,8 @@ echo MEMORYCHIP
 wmic memorychip list
 
 REM Section 2: Check volume, drive fragmentation, and run CHKDSK (scan only).
-echo %y%
+echo %x%
+echo.
 echo DISKDRIVE
 wmic diskdrive list
 echo %y%
@@ -61,21 +79,27 @@ echo CHECK DISK
 chkdsk /scan
 echo %y%
 echo S.M.A.R.T. FAILURE PREDICTION CHECK
-echo NOTE: If not supported by drive(s), results in error: "Not supported"
+echo NOTE: If not supported by drive(s), no output will be logged in this section.
 wmic /namespace:\\root\wmi path MSStorageDriver_FailurePredictStatus
 
 REM Section 3: List available COM devices.
-echo %y%
+echo %x%
+echo.
 mode
 
 REM Section 4: System Event Logs (error or critical only)
-echo %y%
+echo %x%
+echo.
 echo SYSTEM EVENT LOGS
+echo Most recent CRITICAL (Level 1) events:
+WEVTUtil qe System /c:10 /rd:true /f:text /q:"Event[System[(Level=1)]]"
 echo %y%
-WEVTUtil qe System /rd:true /c:25 /f:text /q:"Event[System[(Level=1  or Level=2)]]"
+echo Most recent ERROR (Level 2) events:
+WEVTUtil qe System /c:10 /rd:true /f:text /q:"Event[System[(Level=2)]]"
 
 REM Section 5: Networking information.
-echo %y%
+echo %x%
+echo.
 echo NETWORK INFO
 echo %y%
 echo NETWORK INTERFACE CONFIGURATION
@@ -101,12 +125,14 @@ if /I "%INPUT%"=="y" goto yes
 if /I "%INPUT%"=="n" goto no
 echo Please select 'Y'/'N': & goto Ask
 :yes
+echo.
 echo %y%
 echo Starting System File Check...
 echo NOTE: Depending on the system, this may take a while.
 sfc /verifyonly
 goto cont
 :no
+echo.
 echo %y%
 echo Skipping System File Check...
 :cont
